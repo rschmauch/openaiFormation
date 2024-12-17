1fecfaf808f729b48c7ec57d9c21b7a6d544dfc7
robin <robin.schmauch@laposte.net>
2024-12-17 15:05:50 +0100

    Articles

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ path = "root/pages/"
 pg = st.navigation([
         st.Page(path + "Base.py", title="Bases"),
         st.Page(path + "DallE.py", title="Dall-E"),
+        st.Page(path + "Article.py", title="Article"),
     ]) 
 pg.run()
---
+++ pages/Article.py
@@ -0,0 +1,32 @@
+from openai import OpenAI
+from env import OPENAI_API_KEY
+import streamlit as st
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+value = st.chat_input("Thème de l'article")
+
+with st.chat_message("user"):
+    if (value):
+        txt = st.text("Waiting for API")
+
+        valuetxt = "Create an article about the topic: '"+value+"'. The article must be 200 words long at most."
+        #valueimg = "Create an illustration for an article about the topic: '"+value+"'."
+
+        completion = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[{"role":"user","content":valuetxt}]
+        )
+
+        txt.text(completion.choices[0].message.content)
+
+        valueimg = "illustrate this article:"+completion.choices[0].message.content
+
+        image = client.images.generate(
+                prompt=value,
+                model="dall-e-2",
+                n=2,
+                size="512x512")
+
+        st.image(image.data[0].url)
+        st.image(image.data[1].url)
