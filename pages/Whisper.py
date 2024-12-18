398b4610e1045031d1489bdcb311bcedbd90ad7a
robin <robin.schmauch@laposte.net>
2024-12-18 11:40:33 +0100

    Creation quand fichier de mp3 inexistant

--- pages/Whisper.py
+++ pages/Whisper.py
@@ -2,11 +2,20 @@ from openai import OpenAI
 from env import OPENAI_API_KEY
 import streamlit as st
 from pathlib import Path
+import os
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 audio = st.audio_input("Requete")
 
+dir = Path(__file__).parent.parent / "files"
+
+try:
+    os.mkdir(dir)
+except:
+     if not os.path.isdir(dir):
+          print("Erreur de système")
+          
 
 if (audio):
     file_path = Path(__file__).parent.parent / "files/input.mp3"
@@ -24,7 +33,7 @@ if (audio):
 
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
-        messages=[{"role":"user","content":audiotranscript.text}]
+        messages=[{"role":"user","content":audiotranscript.text+". Fait moi une réponse en quatres phrases"}]
     )
 
     answer = completion.choices[0].message.content
